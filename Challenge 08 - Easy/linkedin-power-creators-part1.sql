2c59eed298417d9a940a1d4a95ef6f7b8a2a05f9
adambushman <adambushman@mail.weber.edu>
2022-10-07 19:24:00 -0600

    Challenge 08 upload

---
+++ Challenge 08 - Easy/linkedin-power-creators-part1.sql
@@ -0,0 +1,18 @@
+/*
+*** Challenge 08 - Easy
+*** LinkedIn Power Creators Part 1 - LinkedIn
+*** Official Submission - Adam Bushman
+*/
+
+-- Simple select statement with a join and where statement for filtering
+SELECT
+  profile_id
+FROM 
+  personal_profiles AS pp
+LEFT JOIN
+  company_pages AS cp ON
+    cp.company_id = pp.employer_id
+WHERE 
+  pp.followers > cp.followers
+ORDER BY
+  profile_id
