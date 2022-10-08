3fc458ff21fecfdd01057935b58e62c28e5e3baa
adambushman <adambushman@mail.weber.edu>
2022-10-07 19:40:59 -0600

    Challenge 25 upload

---
+++ Challenge 25 - Medium/first-transaction.sql
@@ -0,0 +1,24 @@
+/*
+*** Challenge 25 - Medium
+*** First Transaction - Etsy
+*** Official Submission - Adam Bushman
+*/
+
+-- CTE to execute the window function for ranking
+WITH ordered AS (
+  SELECT 
+    *, 
+    RANK() OVER (
+      PARTITION BY user_id
+      ORDER BY transaction_date
+    ) AS rnk
+    FROM
+      user_transactions
+)
+
+-- We get around the filtering for multiple rows of the same user by using distinct
+SELECT 
+  COUNT(DISTINCT user_id)
+FROM ordered
+WHERE rnk = 1
+AND spend >= 50
